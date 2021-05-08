13963c84ca75054267147a0d8cffd4f7484d6588
Ayana <sembai206@gmail.com>
2021-05-08 19:07:38 +0600

    commit

---
+++ beauty-store/src/app/_services/user.service.ts
@@ -0,0 +1,25 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+import {environment} from '@environments/environment';
+import {User} from '@app/_models';
+
+@Injectable({providedIn: 'root'})
+export class UserService {
+  constructor(private http: HttpClient) {
+  }
+
+  getAll() {
+    return this.http.get<User[]>(`${environment.apiUrl}/users`);
+  }
+
+  createUser(username, password, firstName, lastName) {
+    return this.http.post<User[]>(`${environment.apiUrl}/users/add`, {username, password, firstName, lastName});
+  }
+  editUser(id, username, password, firstName, lastName) {
+    return this.http.post<User[]>(`${environment.apiUrl}/users/edit`, {id, username, password, firstName, lastName});
+  }
+  deleteUser(id) {
+    return this.http.post<User[]>(`${environment.apiUrl}/users/delete`, {id});
+  }
+}
